314548fd73fadc99fd187b2caf148248c2d3b302
Daniel Vaini <dnlvaini@gmail.com>
2021-03-20 23:45:44 -0300

    feat: Adding mongoose

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
   const handlebars = require('express-handlebars');
   const app = express();
   const admin = require('./routes/admin');
-  // const mongoose = require('mongoose')
+  const path = require('path')
+  const mongoose = require('mongoose')
 
   
   // Configuração
@@ -14,9 +15,16 @@
       app.engine('handlebars', handlebars({defaultLayout: 'main'}))
       app.set('view engine', 'handlebars')
     // Mongoose
+    mongoose.connect("mongodb://localhost/blogapp").then(() => {
+      console.log('Mongo conected')
+    }).catch((err) => {
+      console.log(err)
+    })
+
+    mongoose.Promise = global.Promise;
       // Em breve
-    //
-  
+    // Public
+      app.use(express.static(path.join(__dirname, 'public')))
 
 // Rotas
   app.use('/admin', admin)
