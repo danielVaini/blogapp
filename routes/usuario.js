66ba65547f80806215dbb608192192dc2a3f595b
Daniel Vaini <dnlvaini@gmail.com>
2021-04-05 23:20:12 -0300

    feat: Creating criptografy for password

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/User')
 const Usuario = mongoose.model('usuarios')
+const bcrypt = require('bcryptjs')
 
 router.get('/registro', (req, res) => {
   res.render('usuarios/registro')
@@ -34,6 +35,43 @@ router.post('/registro', (req, res) => {
 
   }else {
     // Na proxima aula!
+    Usuario.findOne({email: req.body.email}).lean().then((usuario) => {
+      if(usuario) {
+        req.flash('error_msg', "Já exiiste uma conta com este email em nosso sitema")
+        res.redirect('/usuarios/registro')
+      }else {
+
+        const novoUsuario = new Usuario({
+          nome: req.body.nome,
+          email: req.body.email,
+          senha: req.body.senha,
+        })
+
+        bcrypt.genSalt(10, (erro, salt) => {
+          bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+            if(erro){
+              req.flash('error_msg', 'Houve um erro durante o salvamento do usuario')
+              res.redirect('/')
+            }
+
+            novoUsuario.senha = hash;
+
+            novoUsuario.save().then(() => {
+              req.flash('success_msg', 'Usuário criado com sucesso')
+              res.redirect('/')
+            }).catch((erro) => {
+              req.flash('error_msg', 'Houve um erro ao criar o usuário')
+              res.redirect('/usuarios/registro')
+            })
+          })
+        })
+
+
+      }
+    }).catch((err) => {
+      req.flash('error_msg', "Houve um erro interno")
+      res.redirect('/')
+    })
   }
 })
 
