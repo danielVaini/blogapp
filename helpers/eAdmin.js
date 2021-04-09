61763b151c17ce3474653fd9354caddc75e3eb16
Daniel Vaini <dnlvaini@gmail.com>
2021-04-08 23:14:14 -0300

    feat: Creating authenticate for users

---
+++ helpers/eAdmin.js
@@ -0,0 +1,11 @@
+// Verificar se o usuário está autenticado e se ele é admin
+module.exports = {
+  eAdmin: function(req, res, next) {
+    if(req.isAuthenticated() && req.user.eAdmin == 1){
+      return next();
+    }
+
+    req.flash('error_msg', 'Você precisa ser um admin')
+    res.redirect('/')
+  }
+}
